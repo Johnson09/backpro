6a946163aa74f43a5d3e8f8358fd8874fd454b78
Jhon Sotelo <jhonstiven1409@gmail.com>
2021-04-06 10:50:21 -0500

    Configuracion inicial

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,83 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    name: 'Inicio',
+    component: () => import('../views/Inicio.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    component: () => import('../views/Signin.vue')
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/perfil',
+    name: 'Perfil',
+    component: () => import('../views/Perfil.vue')
+  },
+  {
+    path: '/cartera',
+    name: 'Cartera',
+    component: () => import('../views/Cartera.vue')
+  },
+  {
+    path: '/pagos',
+    name: 'Pagos',
+    component: () => import('../views/Pagos.vue')
+  },
+  {
+    path: '/renovacion',
+    name: 'Renovacion',
+    component: () => import('../views/Renovacion.vue')
+  },
+  {
+    path: '/cliente_renovacion',
+    name: 'ClienteRenovacion',
+    component: () => import('../views/ClienteRenovacion.vue')
+  },
+  {
+    path: '/poliza_renovacion',
+    name: 'PolizaRenovacion',
+    component: () => import('../views/PolizaRenovacion.vue')
+  },
+  {
+    path: '/facturacion',
+    name: 'Facturacion',
+    component: () => import('../views/Facturacion.vue')
+  },
+  {
+    path: '/cliente_facturacion',
+    name: 'ClienteFacturacion',
+    component: () => import('../views/ClienteFacturacion.vue')
+  },
+  {
+    path: '/poliza_facturacion',
+    name: 'PolizaFacturacion',
+    component: () => import('../views/PolizaFacturacion.vue')
+  },
+  {
+    path: '/clientes',
+    name: 'Clientes',
+    component: () => import('../views/Clientes.vue')
+  },
+  {
+    path: '/clientes_detalles',
+    name: 'ClientesDetalles',
+    component: () => import('../views/ClientesDetalles.vue')
+  },
+  {
+    path: '/solicitudes',
+    name: 'Solicitudes',
+    component: () => import('../views/Solicitudes.vue')
   },
   {
     path: '/about',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,190 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+      right
+      app
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <template v-for="(item, index) in items">
+          <v-list-item :key="item.title" link>
+            <template>
+              <v-list-item-icon>
+                <v-icon color="blue darken-4">{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-icon color="grey lighten-1">chevron_right</v-icon>
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+          <v-divider
+            v-if="index < items.length - 1"
+            :key="index"
+          ></v-divider>
+        </template>
+        <br>
+        <v-text-field
+          class="ml-8 mr-8"
+          dense
+          outlined
+          label="Powered by"
+          value="Inception company"
+          hint="Version 2.0"
+          persistent-hint
+          disabled
+        >
+        </v-text-field>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar dense dark app color="blue accent-4">
+      <v-flex text-left>
+        <v-toolbar-title>
+          <v-icon>home</v-icon>
+          Dashboard
+        </v-toolbar-title>
+      </v-flex>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <v-container fluid>
+        <v-item-group active-class="primary">
+          <v-container>
+            <v-row>
+              <v-col
+                v-for="modulo in modulos"
+                :key="modulo.title"
+                sm="1"
+                cols="6"
+                md="3"
+              >
+                <v-item>
+                  <v-card
+                    class="d-flex align-center"
+                    dark
+                    color="white"
+                    height="150"
+                  >
+                    <div
+                      class="black--text flex-grow-1 text-center"
+                    >
+                      <v-icon
+                        large
+                        color="blue darken-4"
+                      >
+                        {{modulo.icon}}
+                      </v-icon>
+                      <br>
+                      <label>{{modulo.title}}</label>
+                    </div>
+                  </v-card>
+                </v-item>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-item-group>
+      </v-container>
+    </v-main>
+
+    <v-footer app dense>
+      <v-bottom-navigation
+        absolute
+        color="blue darken-4"
+        shift
+      >
+        <v-btn>
+          <span>Home</span>
+
+          <v-icon color="blue accent-4">mdi-home</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Clientes</span>
+
+          <v-icon color="blue accent-4">mdi-account</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Solicitudes</span>
+
+          <v-icon color="blue accent-4">library_books</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Configuración</span>
+
+          <v-icon color="blue accent-4">settings</v-icon>
+        </v-btn>
+
+        <v-btn
+          @click.stop="drawer = !drawer"
+        >
+          <span>Más</span>
+          <v-icon color="blue accent-4">more_horiz</v-icon>
+        </v-btn>
+      </v-bottom-navigation>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      // Token
+      dialog: false,
+      token: '',
+      datos: {
+        id_perfil: '1',
+        id_modulo: '1'
+      },
+      // Datos usuarios
+      usuario_login: '',
+      usuario_correo: '',
+      perfil_usuario: '',
+      menu_modulos: [],
+      drawer: null,
+      items: [
+        { title: 'Mi perfil', icon: 'account_box' },
+        { title: 'Cartera', icon: 'account_balance_wallet' },
+        { title: 'Renovación', icon: 'create_new_folder' },
+        { title: 'Facturación', icon: 'receipt' },
+        { title: 'Solicitudes', icon: 'library_books' },
+        { title: 'Configuración', icon: 'settings' },
+        { title: 'Soporte', icon: 'live_help' },
+        { title: 'Politica privacidad', icon: 'policy' },
+        { title: 'Salir', icon: 'power_settings_new' }
+      ],
+      modulos: [
+        { title: 'Total polizas', icon: 'account_box' },
+        { title: 'Total clientes', icon: 'account_balance_wallet' },
+        { title: 'Total primas', icon: 'create_new_folder' },
+        { title: 'Renovados', icon: 'receipt' },
+        { title: 'Nuevos clientes', icon: 'library_books' },
+        { title: 'Indicadores', icon: 'settings' }
+      ]
+    }
   }
 }
 </script>
